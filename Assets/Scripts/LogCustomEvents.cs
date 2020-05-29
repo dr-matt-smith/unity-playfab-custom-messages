00b518365f3bb56226a4e5c3942fd2f91a184f59
dr-matt-smith <matt.smith@itb.ie>
2020-05-29 16:36:55 +0100

    added files to project

---
+++ Assets/Scripts/LogCustomEvents.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.Internal;
+
+public class LogCustomEvents : MonoBehaviour 
+{
+    private int _counter = 0;
+
+    // Update is called once per frame
+    void Update () 
+    {
+		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _counter++;
+            String message = _counter + " times";
+           LogPlayFabEvent("SPACE pressed", message);
+            Debug.Log("SPACE pressed " + message);
+        }
+		
+    }
+    
+    private void LogPlayFabEvent(string eventName, string message)
+    {
+        PlayFabClientAPI.WritePlayerEvent(new WriteClientPlayerEventRequest() {
+                Body = new Dictionary<string, object>() {
+                    { "BuiltItBetterEvent", eventName },
+                    { "Message", message }
+                },
+                EventName = "build_it_better_event"
+            },
+            result => Debug.Log("Success"),
+            error => Debug.LogError(error.GenerateErrorReport()));
+    }
+
+
+}
---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,36 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+
+    
+    public void Awake()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId)){
+            /*
+            Please change the titleId below to your own titleId from PlayFab Game Manager.
+            If you have already set the value in the Editor Extensions, this can be skipped.
+            */
+            PlayFabSettings.staticSettings.TitleId = "42";
+        }
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true};
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+    
+
+
+}
